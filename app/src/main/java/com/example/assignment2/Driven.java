a4a1b83a0565bb4a1ff2f1111e766244cb8adb42
Abeer.K <91427896+Abeerkhurram@users.noreply.github.com>
2023-05-07 15:32:13 +0500

    Add Github link to browse repositry

--- app/src/main/java/com/example/assignment2/Driven.java
+++ app/src/main/java/com/example/assignment2/Driven.java
@@ -3,6 +3,7 @@ package com.example.assignment2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,6 +12,7 @@ public class Driven extends AppCompatActivity {
 
 
     Button btn;
+    Button btngit;
     Intent intobj;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +21,17 @@ public class Driven extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_driven);
         btn=findViewById(R.id.activity2);
+        btngit=findViewById(R.id.git);
+        btngit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                intent.setData(Uri.parse("https://github.com/Abeerkhurram/ContactUs_page"));
+                startActivity(intent);
+            }
+        });
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
