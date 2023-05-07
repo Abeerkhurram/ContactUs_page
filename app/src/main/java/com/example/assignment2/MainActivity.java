bff6ee7acb84f41258d5a348d03201a8e187072d
Abeer.K <91427896+Abeerkhurram@users.noreply.github.com>
2023-05-07 19:16:53 +0500

    add whats app functionality

--- app/src/main/java/com/example/assignment2/MainActivity.java
+++ app/src/main/java/com/example/assignment2/MainActivity.java
@@ -3,12 +3,16 @@ package com.example.assignment2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
     ImageView imgbtn;
+    LinearLayout obj;
+    LinearLayout whatsapp;
     Intent intobj;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,13 +21,33 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
         imgbtn=(ImageView) findViewById(R.id.back);
+        whatsapp=(LinearLayout)findViewById(R.id.what_app);
+         obj=(LinearLayout)findViewById(R.id.what_app_usa);
+        obj.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                listener();
+            }
+        });
+        whatsapp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                listener();
+            }
+        });
         imgbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                intobj=new Intent(getApplicationContext(),Driven.class);
-                startActivity(intobj);
-                setContentView(R.layout.activity_driven);
+                finish();
             }
         });
+    }
+    public void listener()
+    {
+        Intent what = new Intent (Intent.ACTION_VIEW);
+        String message ="Hello... How can i help you ??  ";
+        String number = "tel:+923217865622";
+        what.setData(Uri.parse("whatsapp://send?phone=" + number + "&text=" + message));
+        startActivity(what);
     }
 }
