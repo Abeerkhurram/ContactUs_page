85b373caf9d18543c32e9eb2ae0e60722a4eebbc
Abeer.K <91427896+Abeerkhurram@users.noreply.github.com>
2023-05-07 12:46:03 +0500

    Adding title and TextView in linear layout

---
+++ app/src/main/java/com/example/assignment2/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.assignment2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        getSupportActionBar().hide();
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+    }
+}
